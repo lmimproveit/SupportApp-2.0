fac05438abac53d7653b9c338852579917978212
Amanda Yngve <amanda@lmimproveit.com>
2026-08-19 14:53:32 +0200

    Complete Company CRUD with Prisma

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CompanyModule } from './company/company.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [CompanyModule, PrismaModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    CompanyModule,
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/company/company.service.ts
+++ backend/src/company/company.service.ts
@@ -1,26 +1,38 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCompanyDto } from './dto/create-company.dto';
 import { UpdateCompanyDto } from './dto/update-company.dto';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class CompanyService {
+  constructor(private readonly prisma: PrismaService) {}
+
   create(createCompanyDto: CreateCompanyDto) {
-    return 'This action adds a new company';
+    return this.prisma.company.create({
+      data: createCompanyDto,
+    });
   }
 
   findAll() {
-    return `This action returns all company`;
+  return this.prisma.company.findMany();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} company`;
+  return this.prisma.company.findUnique({
+    where: { id },
+  });
   }
 
   update(id: number, updateCompanyDto: UpdateCompanyDto) {
-    return `This action updates a #${id} company`;
+  return this.prisma.company.update({
+    where: { id },
+    data: updateCompanyDto,
+  });
   }
 
   remove(id: number) {
-    return `This action removes a #${id} company`;
-  }
+  return this.prisma.company.delete({
+    where: { id },
+  });
+}
 }
