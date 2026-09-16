d58d457d834fff1e440c98f5550cc9bb2707da50
lmimproveit <github@lmimproveit.com>
2026-09-16 11:13:42 +0200

    Use environment JWT secret in strategy

--- backend/src/auth/jwt.strategy/jwt.strategy.ts
+++ backend/src/auth/jwt.strategy/jwt.strategy.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-  constructor() {
+  constructor(configService: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: 'supportapp-secret-key',
+      secretOrKey: configService.getOrThrow<string>('JWT_SECRET'),
     });
   }
 
